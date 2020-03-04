f20ddb95f6d1fd423f32021c3b0e53c06fe5ac3f
Ankit Kumar <hello@ankdev.me>
2020-03-04 10:43:25 +0530

    mv task1 to gangagsoc/task1

---
+++ gangagsoc/task1/merger.py
@@ -0,0 +1,14 @@
+import os
+def mergefiles(file_list ,output_file):
+
+    f_out = open(output_file,'w')
+    count = 0
+    for f in file_list:
+        f_in = open(f)
+        count = count + int(f_in.read())
+        f_in.close()
+
+    f_out.write(str(count))
+    f_out.flush()
+    f_out.close()
+    return True
