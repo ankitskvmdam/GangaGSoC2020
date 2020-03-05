55f956b03a5ad01244300e10de2778a3b506578d
Ankit Kumar <hello@ankdev.me>
2020-03-06 00:13:15 +0530

    remove unnecessary comments

--- gangagsoc/task1/hello_world_ganga.py
+++ gangagsoc/task1/hello_world_ganga.py
@@ -4,19 +4,10 @@ from monitoring import run_until_state
 
 import os
 
+# This function will create a hello world job Ganga job that executes on Local backend
 def hello_world_ganga():
-
-    """ 
-        This function will create a hello world job Ganga job that executes on Local backend
-    """
     j = Job()
     j.name = "Hello World Job"
-
-    """
-        j.application = Executable()        # By default application is Executable.
-        j.backend = Local()                 # By default application executes on Local backend
-    """
-
     j.submit()
 
     return j
