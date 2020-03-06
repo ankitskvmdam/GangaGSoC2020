7dd22597ef88b137d60b5781aed63d029bfb036e
Ankit Kumar <hello@ankdev.me>
2020-03-06 22:24:16 +0530

    add test for hello_world_ganga

--- test/TestTask1.py
+++ test/TestTask1.py
@@ -5,7 +5,20 @@ import copy
 class TestTask1(unittest.TestCase):
 
     def test_hello_world(self):
-        print('Testing ganga hello world job')
+        from gangagsoc.task1.hello_world_ganga import hello_world_ganga
+        from ganga import Executable, Localhost
+
+        # creating hello world job
+        j = hello_world_ganga()
+
+        # Checking whether job submitted or not
+        self.assertEqual(j.status, 'submitted')
+
+        # Check job backend
+        self.assertEqual(type(j.backend), Localhost)
+
+        # Check job is executable
+        self.assertEqual(type(j.application), Executable)
 
 if __name__ == '__main__':
     unittest.main()
