78faff99d367abc3b48dab7d0ac824f917de9d5f
Ankit Kumar <hello@ankdev.me>
2020-03-08 01:38:59 +0530

    add run.py

---
+++ gangagsoc/task3/gangabackend/run.py
@@ -0,0 +1,12 @@
+import os
+from backend import create_app
+
+app, socket = create_app()
+
+if __name__ == "__main__":
+    socket.run(
+        app,
+        host=os.environ.get("FLASK_HOST", "127.0.0.1"),
+        port=os.environ.get("FLASK_PORT", "9991"),
+        debug=True
+    )
