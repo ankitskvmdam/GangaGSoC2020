d1aa9d691261d552a4cab53933ab1b409f0e9df6
Ankit Kumar <hello@ankdev.me>
2020-03-08 02:00:54 +0530

    add run script to compile frontend

--- gangagsoc/task3/run.sh
+++ gangagsoc/task3/run.sh
@@ -7,14 +7,3 @@ fi
 if [ ! -d "./dist" ]; then
   npm run build
 fi
-
-cd ../backend
-if [ -x "$(command -v pip3)" ]; then # if pip3 executable exists in PATH
-  pip3 install --editable .
-else
-  pip install --editable .
-fi
-
-export FLASK_APP=backend
-export FLASK_DEBUG=true
-flask run
