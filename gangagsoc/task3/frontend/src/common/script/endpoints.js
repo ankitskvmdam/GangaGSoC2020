e3f6345282dccae4a7eed14278d22ad94cdb9808
Ankit Kumar <hello@ankdev.me>
2020-03-09 02:15:19 +0530

    add enpoints

--- gangagsoc/task3/frontend/src/common/script/endpoints.js
+++ gangagsoc/task3/frontend/src/common/script/endpoints.js
@@ -2,7 +2,11 @@
 export const socketEndpoint = 'http://localhost:9991'
 export const socketConnect = 'connect'
 export const socketDisconnect = 'disconnect'
-export const socketCreateJob = 'create job'
-export const socketCreateJobStatus = 'create job status'
 export const socketReceiveJobDetails = 'receive job details'
+export const socketGetJobDetials = 'get job details'
+
 // fetch
+export const fetchEndpoint = 'http://localhost:9991'
+export const apiVersion = '/api/v1'
+export const jobApi = fetchEndpoint + apiVersion + '/job'
+export const submitNewJob = jobApi + '/new/submit'
