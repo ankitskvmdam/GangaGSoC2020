6d8d03c38999528c63f88985bf41447f6ef516b7
Ankit Kumar <hello@ankdev.me>
2020-03-10 05:25:46 +0530

    separeate task 1 part

--- gangagsoc/task1/__init__.py
+++ gangagsoc/task1/__init__.py
@@ -1,8 +1,12 @@
 from .ganga_split_job import run, monitor_job
+from .hello_world_ganga import run_hello_world_ganga_with_output
 
-def run_task():
+def run_task_part_2():
     j = run()
     monitor_job(j)
+
+def run_task_part_1():
+    run_hello_world_ganga_with_output()
 
 def run_test():
     import pytest
