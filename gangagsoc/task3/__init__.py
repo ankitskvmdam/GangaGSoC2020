913685efb71301d13bbee26c7a1830ac7c4d6857
Ankit Kumar <hello@ankdev.me>
2020-03-10 21:45:45 +0530

    build process refactor

--- gangagsoc/task3/__init__.py
+++ gangagsoc/task3/__init__.py
@@ -2,58 +2,61 @@ from . import frontend
 from . import gangabackend
 import os
 
-def output_reader(proc):
-    for line in iter(proc.stdout.readline, b''):
-        print('INFO: {0}'.format(line.decode('utf-8')), end='')
+def build_frontend():
+    from plumbum import local
+    frontend_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "frontend")
+    os.chdir(frontend_path)
 
+    npm = local['npm']
+
+    print('Installing dependencies.')
+    print('Command running: npm install')
+    print('Please wait...')
+    
+    # > npm install
+    npm('install')
+
+    print('Installation completed!')
+
+    print('Building frontend')
+    print('Command running: npm run build')
+    print('Please wait...')
+    
+    # > npm run build
+    npm('run', 'build')
+    
+    print('Completed!')
+
+# Build frontend and backend
 def run_task():
     import subprocess
     import threading
+    
 
-    frontend_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "frontend")
     frontend_dist_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "frontend", "dist")
     backend_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "gangabackend")
-
-    os.chdir(frontend_path)
-
-    proc1 = subprocess.Popen(["npm", "install"],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-    
-    t1 = threading.Thread(target=output_reader, args=(proc1,))
-    t1.start()
-    t1.join()
-    proc1.wait()
-
-    proc2 = subprocess.Popen(["npm", "run", "build"],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-
-    t2 = threading.Thread(target=output_reader, args=(proc2,))
-    t2.start()
-    t2.join()
-    proc2.wait()
-
+ 
+    build_frontend()
+    print('Starting server, Please wait..')
     os.chdir(frontend_dist_path)
-    proc3 = subprocess.Popen(["python", "server.py"],
-                            stdout=subprocess.PIPE,
+    proc1 = subprocess.Popen(["python", "server.py"],
                             stderr=subprocess.STDOUT)
                             
-    t3 = threading.Thread(target=output_reader, args=(proc3,))
-    t3.start()
+    t1 = threading.Thread(target=proc1.communicate, args=(proc1,))
+    t1.start()
 
     os.chdir(backend_path)
-    proc4 = subprocess.Popen(["python", "run.py"],
-                            stdout=subprocess.PIPE,
+    proc2 = subprocess.Popen(["python", "run.py"],
                             stderr=subprocess.STDOUT)
 
-    t4 = threading.Thread(target=output_reader, args=(proc4,))
-    t4.start()
+    t2 = threading.Thread(target=proc2.communicate, args=(proc2,))
+    t2.start()
     
-    t3.join()
-    t4.join()
+    t1.join()
+    t2.join()
 
 
+# run test
 def run_test():
     import pytest
     import os
