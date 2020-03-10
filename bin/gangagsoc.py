6dddc794872e2875125a9169a2500a417fb0a486
Ankit Kumar <hello@ankdev.me>
2020-03-10 06:18:37 +0530

    task1 test and sub task separate

--- bin/gangagsoc.py
+++ bin/gangagsoc.py
@@ -7,29 +7,29 @@ def cli():
     pass
 
 @cli.command(help='Run task 1 Part 1: Hello world task')
-@click.option('--test', is_flag=True, default=False, help='Run tests for task 1')
-def task1_1(test):
-    if test:
-        run_test1()
-    else:
-        run_task_part_1()
+def task1_1():
+    run_task_part_1()
 
 @cli.command(help='Run task 1 Part 2: Ganga split job')
-@click.option('--test', is_flag=True, default=False, help='Run tests for task 1')
-def task1_2(test):
-    if test:
-        run_test1()
-    else:
-        run_task_part_2()
+def task1_2():
+    run_task_part_2()
 
 
 @cli.command(help='Run task 3: Ganga GUI task')
-@click.option('--test', is_flag=True, default=False, help='Run tests for task 3')
-def task3(test):
-    if test:
-        run_test3()
-    else:
+def task3():
         run_task3()
 
+@cli.command(help='Run test, if no flag given then both task test will run')
+@click.option('--task1', is_flag=True, default=False, help='Run tests for task 1')
+@click.option('--task3', is_flag=True, default=False, help='Run tests for task 3')
+def test(task1, task3):
+    if task1:
+        run_test1()
 
+    elif task3:
+        run_test3()
+
+    else:
+        run_test1()
+        run_test3()
 
