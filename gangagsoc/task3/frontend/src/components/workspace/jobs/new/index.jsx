cb7827beb144c99878ea4e67cf14ff4747460e33
Ankit Kumar <hello@ankdev.me>
2020-03-11 11:17:17 +0530

    warning msg update

--- gangagsoc/task3/frontend/src/components/workspace/jobs/new/index.jsx
+++ gangagsoc/task3/frontend/src/components/workspace/jobs/new/index.jsx
@@ -13,7 +13,7 @@ class Index extends React.Component{
         return(
             <div className='container-wrapper'>
                 <ContainerHeading heading="Create New Job" />
-                <MsgSm type="warning" msg="*Please don't change page after submitting job. You may lost the progress. Switching between different page without losing information will be shiped once the project is complete" />
+                <MsgSm type="warning" msg="*Please don't change page until job submitted successfully (Successful message will be displayed). Otherwise you may lost the progress. Switching between different page without losing information will be shiped once the project is complete" />
                 <Form />
                 <JobsDetails />
             </div>
